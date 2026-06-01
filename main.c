72c934b5cb07d4ee2a42be233a2926aec9f53c49
niv.kirshenbaum <niv.kirshenbaum@post.runi.ac.il>
2026-06-01 18:32:58 +0300

    f c

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 int main() {
 
+    // Part 1
     int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if (sock == -1) {
         perror("socket");
@@ -26,4 +27,15 @@ int main() {
     }
 
     printf("\n");
+
+    // Part 2
+    struct ethhdr *eth = (struct ethhdr *)buffer;
+
+    printf("----------------------------------------\n");
+    printf("mac destination: %02x:%02x:%02x:%02x:%02x:%02x\n", eth->h_dest[0], eth->h_dest[1], eth->h_dest[2], eth->h_dest[3], eth->h_dest[4], eth->h_dest[5]);
+    printf("mac source: %02x:%02x:%02x:%02x:%02x:%02x\n", eth->h_source[0], eth->h_source[1], eth->h_source[2], eth->h_source[3], eth->h_source[4], eth->h_source[5]);
+    printf("EtherType: %04x\n", ntohs(eth->h_proto));
+
+    // Part 3
+    
 }
