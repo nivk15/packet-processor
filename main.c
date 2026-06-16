052b51a8c320718fb31e1eed65eabfa73432e075
niv.kirshenbaum <niv.kirshenbaum@post.runi.ac.il>
2026-06-16 16:51:56 +0300

    finish part 5

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>           // printf
 #include <stdlib.h>          // exit
+#include <string.h>
 #include <sys/socket.h>      // socket, recvfrom
 #include <netinet/in.h>      // htons
 #include <linux/if_ether.h>  // ETH_P_ALL
@@ -34,12 +35,12 @@ int main() {
         if (ntohs(eth->h_proto) != 0x0800) continue;           // 0x0800 is the code for IPv4
 
 
-        printf("----------------------------------------\n");
-        printf("Ethernet header:\n");
-        printf("mac destination: %02x:%02x:%02x:%02x:%02x:%02x\n", eth->h_dest[0], eth->h_dest[1], eth->h_dest[2], eth->h_dest[3], eth->h_dest[4], eth->h_dest[5]);
-        printf("mac source: %02x:%02x:%02x:%02x:%02x:%02x\n", eth->h_source[0], eth->h_source[1], eth->h_source[2], eth->h_source[3], eth->h_source[4], eth->h_source[5]);
-        printf("EtherType: %04x\n", ntohs(eth->h_proto));
-        printf("----------------------------------------\n");
+        // printf("----------------------------------------\n");
+        // printf("Ethernet header:\n");
+        // printf("mac destination: %02x:%02x:%02x:%02x:%02x:%02x\n", eth->h_dest[0], eth->h_dest[1], eth->h_dest[2], eth->h_dest[3], eth->h_dest[4], eth->h_dest[5]);
+        // printf("mac source: %02x:%02x:%02x:%02x:%02x:%02x\n", eth->h_source[0], eth->h_source[1], eth->h_source[2], eth->h_source[3], eth->h_source[4], eth->h_source[5]);
+        // printf("EtherType: %04x\n", ntohs(eth->h_proto));
+        // printf("----------------------------------------\n");
         //--------------------------------------------------------------------------------
 
         // Part 3  (parsing IP header)
@@ -52,42 +53,52 @@ int main() {
         unsigned char *dst = (unsigned char *)&(ip->daddr);
 
 
-        for (int i = 0; i < packet_size; i++){
-            printf("%02x ", buffer[i]);
-        }
-        printf("\n");
+        // for (int i = 0; i < packet_size; i++){
+            // printf("%02x ", buffer[i]);
+        // }
+        // printf("\n");
 
-        printf("----------------------------------------\n");
-        printf("IP header:\n");
-        printf("version: %d\n", ip->version);
-        printf("ihl: %d\n", ip->ihl);
-        printf("tos: %02x\n", ip->tos);
-        printf("tot_len: %d\n", ntohs(ip->tot_len));
-        printf("id: %04x\n", ntohs(ip->id));
-        printf("frag_off: %04x\n", ntohs(ip->frag_off));
-        printf("ttl: %d\n", ip->ttl);
-        printf("protocol: %d\n", ip->protocol);
-        printf("check: %04x\n", ntohs(ip->check));
-        printf("saddr: %d.%d.%d.%d\n", src[0], src[1], src[2], src[3]);
-        printf("daddr: %d.%d.%d.%d\n", dst[0], dst[1],dst[2], dst[3]);
-        printf("----------------------------------------\n");
-        //--------------------------------------------------------------------------------
+        // // printf("----------------------------------------\n");
+        // // // printf("IP header:\n");
+        // // printf("version: %d\n", ip->version);
+        // // printf("ihl: %d\n", ip->ihl);
+        // // printf("tos: %02x\n", ip->tos);
+        // // printf("tot_len: %d\n", ntohs(ip->tot_len));
+        // // printf("id: %04x\n", ntohs(ip->id));
+        // // printf("frag_off: %04x\n", ntohs(ip->frag_off));
+        // // printf("ttl: %d\n", ip->ttl);
+        // // printf("protocol: %d\n", ip->protocol);
+        // // printf("check: %04x\n", ntohs(ip->check));
+        // // printf("saddr: %d.%d.%d.%d\n", src[0], src[1], src[2], src[3]);
+        // // printf("daddr: %d.%d.%d.%d\n", dst[0], dst[1],dst[2], dst[3]);
+        // // printf("----------------------------------------\n");
+        // --------------------------------------------------------------------------------
 
         // Part 4  (parsing TCP & UDP headers to get source and destination ports)
+        int sport = 0, dport = 0;
+        char proto[16];
+
         if (ip->protocol == 6) {
             struct tcphdr *tcp = (struct tcphdr *)(buffer + 14 + ip->ihl * 4);      // grep -A 20 "struct tcphdr" /usr/include/linux/tcp.h
-            printf("Source port: %d\n", ntohs(tcp->source));
-            printf("Dest port: %d\n", ntohs(tcp->dest));
-        }
-        if (ip->protocol == 17) {
+            sport = ntohs(tcp->source);
+            dport = ntohs(tcp->dest);
+            strcpy(proto, "TCP");
+        } else if (ip->protocol == 17) {
             struct udphdr *udp = (struct udphdr *)(buffer + 14 + ip->ihl * 4);     // grep -A 10 "struct udphdr" /usr/include/linux/udp.h 
-            printf("Source port: %d\n", ntohs(udp->source));
-            printf("Dest port: %d\n", ntohs(udp->dest));
+            sport = ntohs(udp->source);
+            dport = ntohs(udp->dest);
+            strcpy(proto, "UDP");
+        } else {
+            sprintf(proto, "PROTO(%d)", ip->protocol);
         }
+        //--------------------------------------------------------------------------------
 
-
-
-        printf("-----------------------------------------------\n");
+        // Part 5  (single-line packet summary)
+        printf("%d.%d.%d.%d:%d -> %d.%d.%d.%d:%d %s %d bytes\n",
+            src[0], src[1], src[2], src[3], sport,
+            dst[0], dst[1], dst[2], dst[3], dport,
+            proto, ntohs(ip->tot_len)
+        );
     }
 }
  
